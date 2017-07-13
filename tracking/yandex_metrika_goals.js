81e449c664f1de2bd680f60642cbb1ed9a44b62b
Konstantin Rudenok <const.stuff@gmail.com>
2017-07-13 09:27:45 +0300

    Create yandex_metrika_goals.js

---
+++ tracking/yandex_metrika_goals.js
@@ -0,0 +1,17 @@
+	var counterId = 'PlaceCounterIdHere';
+	var goalId = 'PlaceJSGoalIdentifierHere';
+
+	var m_checker_firstscreen = 0;
+	var m_interval_firstscreen = setInterval(function(){
+		if(typeof window['yaCounter' + counterId] == 'object' && !m_checker_firstscreen)
+		{
+			window['yaCounter' + counterId].reachGoal(goalId);
+			console.log(
+				'%c  Counter: ' + '%c' + counterId + ' ' + '%cfired successfully for Goal: ' + '%c' + goalId + '  ', 
+				'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold', 
+				'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold'
+			);
+			m_checker_firstscreen = 1;
+			clearInterval(m_interval_firstscreen);
+		}
+	}, 1000);
