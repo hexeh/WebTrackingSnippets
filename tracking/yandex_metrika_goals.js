38916ef7e1450b2918f9700fd10b8451d03afd2e
Konstantin Rudenok <const.stuff@gmail.com>
2017-07-26 21:07:59 +0300

    Update yandex_metrika_goals.js

--- tracking/yandex_metrika_goals.js
+++ tracking/yandex_metrika_goals.js
@@ -5,7 +5,12 @@
 	var m_interval_firstscreen = setInterval(function(){
 		if(typeof window['yaCounter' + counterId] == 'object' && !m_checker_firstscreen)
 		{
-			window['yaCounter' + counterId].reachGoal(goalId, function(){
+			var sendGoalParams = 
+			{
+				'fired_through': 'GTM',
+				'time': date.now()
+			}
+			window['yaCounter' + counterId].reachGoal(goalId, sendGoalParams, function(){
 				console.log(
 					'%c  Counter: ' + '%c' + counterId + ' ' + '%cfired successfully for Goal: ' + '%c' + goalId + '  ', 
 					'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold', 
