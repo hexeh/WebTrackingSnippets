c5173100fe0d164b9e478d88b914228d76275214
Konstantin Rudenok <const.stuff@gmail.com>
2017-07-26 21:05:23 +0300

    Update yandex_metrika_goals.js

--- tracking/yandex_metrika_goals.js
+++ tracking/yandex_metrika_goals.js
@@ -5,12 +5,13 @@
 	var m_interval_firstscreen = setInterval(function(){
 		if(typeof window['yaCounter' + counterId] == 'object' && !m_checker_firstscreen)
 		{
-			window['yaCounter' + counterId].reachGoal(goalId);
-			console.log(
-				'%c  Counter: ' + '%c' + counterId + ' ' + '%cfired successfully for Goal: ' + '%c' + goalId + '  ', 
-				'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold', 
-				'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold'
-			);
+			window['yaCounter' + counterId].reachGoal(goalId, function(){
+				console.log(
+					'%c  Counter: ' + '%c' + counterId + ' ' + '%cfired successfully for Goal: ' + '%c' + goalId + '  ', 
+					'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold', 
+					'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold'
+				);
+			});
 			m_checker_firstscreen = 1;
 			clearInterval(m_interval_firstscreen);
 		}
