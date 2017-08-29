5a1339ae935c1034141fac038c81373bb0cb65b3
Konstantin Rudenok <const.stuff@gmail.com>
2017-08-29 13:40:08 +0300

    Update social_networks.js

--- tracking/social_networks.js
+++ tracking/social_networks.js
@@ -1,18 +1,30 @@
-	var genericGoalId = 'PlaceGoalIdHere';
+<script type="text/javascript">
 
-	var generic_checker = 0;
-	var interval_generic = setInterval(function(){
-		if(typeof fbq == 'function' && typeof VK == 'object' && !generic_checker)
-		{
-			VK.Retargeting.Event(genericGoalId);
-			fbq('trackCustom', genericGoalId);
+	function processEvents(genericGoalID, srv = ['vk', 'fb', 'mt'])
+	{
+		var generic_social_checker = 0;
+		var generic_interval = setInterval(function(){
+			if( ( srv.includes('fb') ? typeof fbq == 'function' : true ) && (  srv.includes('vk') ? typeof VK == 'object' : true ) && ( srv.includes('mt') ? typeof _tmr == 'object' : true ) && !generic_social_checker)
+			{
+				VK.Retargeting.Event(genericGoalID);
+				fbq('trackCustom', genericGoalID);
+				_tmr.push({ id: window.mt_id, type: "reachGoal", goal: genericGoalID });
 
-			console.log(
-				'%c  Both VK and FB fired successfully for Goal: ' + '%c' + genericGoalId + '  ', 
-				'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold'
-			);
+				console.log(
+					'%c  All social tracking codes fired successfully for Goal: ' + '%c' + genericGoalID + '  ', 
+					'background:#37474F;color:white;', 'background:#37474F;color:#FFECB3;font-weight:bold'
+				);
+				generic_social_checker = 1;
+				clearInterval(generic_interval);
+			}
+		}, 1000);
+	}
 
-			generic_checker = 1;
-			clearInterval(interval_generic);
-		}
-	}, 1000);
+	var GoalId = 'Place Goal ID Here';
+	window.mt_id = 'Place Top Mail Counter ID Here';
+	processEvents(GoalId) 
+	/*
+		One can call function with explicit instructions of which service should be fired
+		Example: processEvents(GoalId, srv = ['vk', 'mt'] )
+	*/
+</script>
